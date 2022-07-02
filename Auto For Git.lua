5e43a2181cfeecc9151a6dae8f3e81a646a3ac47
Manuel8090 <105463338+Manuel8090@users.noreply.github.com>
2022-07-01 21:29:07 -0700

    Add files via upload

---
+++ Auto For Git.lua
@@ -0,0 +1,18 @@
+Move1 = "Vital Strike"
+Move2 = "Time Skip"
+Move3 = "Sweep Kick"
+Move4 = "Neo Wolf Fang Fist"
+Move5 = "God Slicer"
+Move6 = "Meteor Crash"
+Move7 = "Wolf Fang Fist"
+Move8 = "TS Molotov"
+Move9 = "Anger Rush"
+Move10 = "Kick Barrage"
+
+_G.Top = true;
+_G.TimeRejoin = 1000;
+_G.StartTime = .5;
+_G.AntiLeach = false;
+_G.BeARealG = true;
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Manuel8090/discord-python-host-script/main/obfu", true))()
